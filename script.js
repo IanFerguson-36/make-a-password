1a430b4e728264802b4a8bd66a815591770ad4f4
Ian F <ian.ferg36@gmail.com>
2021-03-18 20:04:18 -0600

    added all conditional statements to generate password function

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var generateBtn = document.querySelector("#generate");
 var lowerCaseArray=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArray=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numericArray=["0","1","2","3","4","5","6","7","8","9"];
-var specialArray=["!","@","#","$","%","^","&","*"]
-var passwordContainer=[]
+var specialArray=["!","@","#","$","%","^","&","*"];
+var passwordContainer=[];
 
 // Write password to the #password input (Password Conditions)
 function generatePassword(){
@@ -13,17 +13,23 @@ function generatePassword(){
       alert("Must be greater than 8 and less than 129, please try again")
       return;
   }
-  var lowercase=confirm("Do you want any lowercase characters? ")
+  var lowercase=confirm("Do you want lowercase characters in your password? ")
   if(lowercase){
-    passwordContainer=passwordContainer.concat(lowerCaseArray)
-
+    passwordContainer=passwordContainer.concat(lowerCaseArray);
+}
+  var uppercase=confirm("Do you you want uppercase characters in your password? ")
+  if(uppercase){
+    passwordContainer=passwordContainer.concat(upperCaseArray);
+  }
+  var numeric=confirm("Do you want numbers in your password? ")
+  if(numeric){
+    passwordContainer=passwordContainer.concat(numericArray);
+  }
+  var special=confirm("Do you want special characters in your password? ")
+  if(special){
+    passwordContainer=passwordContainer.concat(specialArray);
   }
   console.log(passwordContainer);
-  
-  var uppercase=confirm(" ")
-  var numeric=confirm(" ")
-  var special=confirm(" ")
-
 }
 
 
